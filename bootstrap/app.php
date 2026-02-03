4033a744205681efa583b1bfbe303dd0d6a5f7aa
Ayan <karmakarayan217@gmail.com>
2026-02-04 00:45:16 +0530

    trust proxy added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminOnly::class,
         ]);
+          $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
